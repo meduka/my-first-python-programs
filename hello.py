8acf0383c7eaac8bec07382bd3a5976f5a070bad
meduka <31477336+meduka@users.noreply.github.com>
2017-08-30 09:09:09 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+#This program says hello and greets a person by name.
+#
+#Manuela Cano
+#Aug 24, 2017
+
+print("Hello.")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+
+print("Where where you born, " + name + "?")
+birthplace = input()
+print("That's interesting. I'd like to visit " + birthplace + "one day.")
