8b0966e0c89d7eeafc3d7b897eec83bb471650c7
meduka <31477336+meduka@users.noreply.github.com>
2017-08-31 09:08:13 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,68 @@
+print("Please choose things(plural).")
+
+things_plural1 = input()
+
+
+print("Now please choose an adjective.")
+
+adjective2 = input()
+
+
+print("Choose a song title.")
+
+song_title3 = input()
+
+
+print("Name a celebrity.")
+
+celebrity4 = input()
+
+
+print("Please name a feeling.")
+
+feeling5 = input()
+
+
+print("Now choose a verb")
+
+verb6 = input()
+
+
+print("Choose a place.")
+
+place7 = input()
+
+
+print("Please choose a type of food.")
+
+food8 = input()
+
+
+print("Please choose things(plural) again.")
+
+things_plural9 = input()
+
+
+print("Finally, choose a person.")
+
+person10 = input()
+
+
+
+print("")
+
+print("Here's your madlib!")
+
+print("")
+print("")
+
+print("I just got back from a pizza party with " + person10 + ". Can you believe we got to eat " + adjective2 +
+      " pizza in " + place7 + "?! Everyone got to choose their own toppings.")
+print("I made " + food8 + " and " + things_plural1 + " pizza, which is my favorite! They even stuffed the crust with " +
+      things_plural9 + ". How " + feeling5 + "! If that wasn't good enough already,")
+print(celebrity4 + " was there singing " + song_title3 + " I was so inspired by the music, I had to get up out of my seat and" + verb6)
+
+
+
+
+
